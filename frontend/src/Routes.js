d561da8732fe6cbf95180fff05d254b3f3ee0d85
Alisha Fatima <alishafatima6768@gmail.com>
2026-04-30 07:05:58 +0500

    go ahead

--- frontend/src/Routes.js
+++
@@ -1,9 +0,0 @@
-// import LoginPage from "./pages/auth/LoginPage";
-import { ActivationPage } from "./pages";
-// import SignUpPage from "./pages/auth/SignupPage";
-
-export {
-    // LoginPage,
-    // SignUpPage,
-    ActivationPage,
-}
--- frontend/src/server.js
+++
@@ -1,2 +0,0 @@
-export const server = "/api/v2";
-export const backend_url = "/";
